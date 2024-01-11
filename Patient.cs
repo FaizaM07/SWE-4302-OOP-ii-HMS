14553862963ebf2d47ee478055cac25e3c599450
Faiza Maliat <122308040+FaizaM07@users.noreply.github.com>
2024-01-12 00:51:40 +0600

    Add files via upload

--- Patient.cs
+++ Patient.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System
 {
-    internal class Patient
+    class Patient
     {
+        public int PatientID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+
+        public string Disease { get; set; }
     }
 }
