f0ae2f3ccb53f0fa134d82e2c2dd94248424c879
Faiza Maliat <122308040+FaizaM07@users.noreply.github.com>
2024-01-12 00:51:21 +0600

    doctor's

--- Doctor.cs
+++ Doctor.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System
 {
-    internal class Doctor
+     class Doctor
     {
+
+        public int DoctorID { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public string ContactNumber { get; set; }
+
+
+
+
+
     }
 }
